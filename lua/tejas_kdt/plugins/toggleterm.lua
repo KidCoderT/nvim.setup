afb7e0e6faddb40ae5559bdbc15651a68f5ed363
Tejas <tejas.sunil.75o25@gmail.com>
2025-05-22 15:58:09 +0530

    fixed toggle term keybinding

--- lua/tejas_kdt/plugins/toggleterm.lua
+++ lua/tejas_kdt/plugins/toggleterm.lua
@@ -8,6 +8,20 @@ return {
 			require("toggleterm").setup()
 			local Terminal = require("toggleterm.terminal").Terminal
 
+			-- Move even inside the terminal
+			function _G.set_terminal_keymaps()
+				local opts = { buffer = 0 }
+				vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+				vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+				vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+				vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+				vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+			end
+
+			vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
 			-- Toggalable Terminals
 
 			local horiz_terminal = Terminal:new({
@@ -40,9 +54,12 @@ return {
 				floating_terminal:toggle()
 			end
 
-			vim.keymap.set("n", "<C-h>", toggle_h, { noremap = true, silent = true })
-			vim.keymap.set("n", "<C-j>", toggle_v, { noremap = true, silent = true })
-			vim.keymap.set("n", "<C-k>", toggle_f, { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>th", toggle_h, { noremap = true, silent = true })
+			vim.keymap.set("n", "<A-h>", toggle_h, { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>tv", toggle_v, { noremap = true, silent = true })
+			vim.keymap.set("n", "<A-v>", toggle_v, { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>tf", toggle_f, { noremap = true, silent = true })
+			vim.keymap.set("n", "<A-f>", toggle_f, { noremap = true, silent = true })
 
 			-- LazyGit in LUA
 			local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
